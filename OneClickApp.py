3c5216cb10f9867179cb05db84bb5bb743e1bb78
Razi <razibhatti1@gmail.com>
2019-07-18 11:38:45 +0500

    Added unfollow specific amount

--- OneClickApp.py
+++ OneClickApp.py
@@ -39,6 +39,8 @@ class oneClickApp:
         userTag = "js-username-field"
         passwordTag = "js-password-field"
         try:
+            WebDriverWait(beginBot, 10).until(
+                EC.presence_of_all_elements_located)
             user = WebDriverWait(beginBot, 10).until(
                 EC.presence_of_element_located((By.CLASS_NAME, userTag)))
             password = WebDriverWait(beginBot, 10).until(
@@ -47,6 +49,7 @@ class oneClickApp:
             user.send_keys(self.username)
             password.clear()
             password.send_keys(self.password)
+            time.sleep(1)
             login_button = beginBot.find_element_by_css_selector(
                 'button.submit.EdgeButton.EdgeButton--primary.EdgeButtom--medium')
             login_button.submit()
@@ -74,6 +77,7 @@ class oneClickApp:
             print(error)
 
     def mass_follow_twitter(self, keywords):
+        start_time = time.time()
         beginBot = self.beginBot
         beginBot.get('https://twitter.com/search?q=' + keywords + '&src=typd')
         self.scroll_down()  # Scrolling to end of page for max tweets
@@ -90,16 +94,17 @@ class oneClickApp:
                         EC.presence_of_all_elements_located)
                     beginBot.find_element(
                         By.XPATH, "//span[text() = 'Follow']").click()
-                    print(inner)
                     time.sleep(2)
+                    print("Followed: " + links[inner].split("/")[1] + " " + str(inner + 1))
                 except Exception as error:
                     print(error)
 
         except Exception as error:
             print(error)
+        print("Time taken: " + str(int((time.time() - start_time) / 60)) + " minutes")
         beginBot.get('https://twitter.com/')
 
-    def unfollow_all(self):
+    def get_followed_users(self):
         beginBot = self.beginBot
         beginBot.get('https://twitter.com/following')
         try:
@@ -110,22 +115,50 @@ class oneClickApp:
             username = []
             for users in soup.find_all("b", class_="u-linkComplex-target"):
                 username.append(users.text)
-            userprofile = []
-            for users in soup.find_all("a", class_="fullname ProfileNameTruncated-link u-textInheritColor js-nav"):
-                userprofile.append(users.text.strip())
             username.pop(0)
             username.pop(0)
-            userprofile.pop(0)
-            ##
+            return username
+        except Exception as error:
+            print(error)
+
+    def unfollow_all(self):
+        try:
+            self.unfollow_specific_amount(1, 1)
+        except Exception as error:
+            print(error)
+
+    def unfollow_specific_amount(self, amount, flag=0):
+        start_time = time.time()
+        unfollowed = 0
+        beginBot = self.beginBot
+        try:
+            username = self.get_followed_users()
+            if flag == 1:
+                amount = len(username)
+            for user in username:
+                if unfollowed == amount:
+                    break
+                beginBot.get('https://twitter.com/' + user)
+                WebDriverWait(beginBot, 10).until(
+                    EC.presence_of_all_elements_located)
+                beginBot.find_element_by_xpath(
+                    '//div[@class = "UserActions   u-textLeft"]//span[contains(@class, "user-actions-follow-button '
+                    'js-follow-btn follow-button")]').click()
+                unfollowed += 1
+                time.sleep(2)
+                print("Unfollowed: " + user + " " + str(unfollowed))
 
         except Exception as error:
             print(error)
+        print("Time taken: " + str(int((time.time() - start_time) / 60)) + " minutes")
         beginBot.get('https://twitter.com/')
 
 
-bot = oneClickApp("twitter", "user", "password")
-if bot.login() == -1:
-    print("Wrong Input")
-time.sleep(5)
-bot.mass_follow_twitter("doraemon")
-#bot.unfollow_all()
+if __name__ == "__main__":
+    username = input("Input Username: ")
+    password = input("Input Password: ")
+    bot = oneClickApp("twitter", username, password)
+    bot.login()
+    time.sleep(2)
+    bot.mass_follow_twitter("elon musk")
+    # bot.unfollow_all()
